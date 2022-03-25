cbeb8f14624eb8e75695e6ef3cbe64cf2cd890c7
akcheu <akcheung@ucsd.edu>
2022-03-25 12:21:10 -0700

    Change

--- script.js
+++ script.js
@@ -73,3 +73,7 @@ $(document).ready(function(){
         }
     });
 });
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
